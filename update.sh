82c28a2dcf53f8ef2dc1ca1ff900fce5f9e4fc8b
Kenji Saito <ken-yo@mbr.nifty.com>
2023-10-19 22:12:16 +0900

    Bumps node modules

---
+++ update.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+CUR=$(pwd)
+
+CURRENT=$(cd $(dirname $0);pwd)
+echo "${CURRENT}"
+
+cd "${CURRENT}/app"
+git pull --prune
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+echo ""
+pwd
+pnpm install && pnpm up && pnpm lint-fix && pnpm build
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+
+cd "${CURRENT}/cdk"
+git pull --prune
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+echo ""
+pwd
+pnpm install && pnpm up && pnpm lint-fix && pnpm build
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+git commit -am "Bumps node modules" && git push
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+
+cd "${CUR}"
